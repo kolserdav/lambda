38ee3c0e21612d6e8917a99f1a435b753cfd08b5
kolserdav <serega12101983@gmail.com>
2018-04-01 19:23:37 +0800

    save-dev

---
+++ template/test.template.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+
+</head>
+
+<body>
+
+{{ test2 }} or {{ test3 }}
+
+<? echo 'test php'; ?>
+@field2
+{%   for value in array2 %} {% value  %} {% endfor  %}
+
+{%   for value in array3 %} {% value  %} {% endfor  %}
+</body>
+</html>
